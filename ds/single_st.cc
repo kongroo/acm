28480c7602da57da8ef647e87b066c79e741b032
kongroo <imjcqt@gmail.com>
2018-10-22 14:17:08 +0800

    .

--- ds/single_st.cc
+++ ds/single_st.cc
@@ -3,13 +3,12 @@ using namespace std;
 
 
 template <typename T> struct SingleST {
-    T unit; // op(x, unit) = op(unit, x) = x
     size_t n;
     vector<T> V;
     using Func = function<T(T, T)>;
     Func op, alter;
-    SingleST(const vector<T>& A, T unit = T(), Func op = plus<T>(), Func alter = plus<T>()
-            ): unit(unit), n(A.size()), V(n * 2, unit), op(op), alter(alter) {
+    SingleST(const vector<T>& A, Func op = plus<T>(), Func alter = plus<T>()
+            ): n(A.size()), V(n * 2), op(op), alter(alter) {
         copy(A.begin(), A.end(), V.begin() + n);
         for (int i = (int)n - 1; i > 0; i--) V[i] = op(V[i << 1], V[i << 1 | 1]);
     }
@@ -18,12 +17,14 @@ template <typename T> struct SingleST {
             V[p >> 1] = op(V[(p | 1) - 1], V[p | 1]);
     }
     T query(int l, int r) {
-        T left = unit, right = unit;
+        assert(l < r);
+        T left, right;
+        bool b1 = false, b2 = false;
         for (l += n, r += n; l < r; l >>= 1, r >>= 1) {
-            if (l & 1) left = op(left, V[l++]);
-            if (r & 1) right = op(V[--r], right);
+            if (l & 1) left = b1 ? op(left, V[l++]) : V[l++], b1 = true;
+            if (r & 1) right = b2 ? op(V[--r], right) : V[--r], b2 = true;
         }
-        return assert(l < r), op(left, right);
+        return !b1 ? right : !b2 ? left : op(left, right);
     }
 };
 
