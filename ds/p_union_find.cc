d60b86fb43bad5b91a3af9817a08febf00dba515
kongroo <imjcqt@gmail.com>
2018-11-02 00:49:36 +0800

    add persistent union find

--- ds/p_union_find.cc
+++ ds/p_union_find.cc
@@ -1,17 +1,28 @@
 #include <bits/stdc++.h>
+#include <ext/rope>
 using namespace std;
+using namespace __gnu_cxx;
+
 
 struct PUnionFind {
-    vector<int> V, Rt;
-    PUnionFind(int n): V(n, -1) {}
-    int root(int x) { return V[x] < 0 ? x : V[x] = root(V[x]); }
-    int size(int x) { return -V[root(x)]; }
-    bool same(int x, int y) { return root(x) == root(y); }
+    vector<rope<int>> F;
+    PUnionFind(int n): F(1, rope<int>(n, -1)) {}
+    int root(int x, int t = -1) {
+        if (t < 0) t = (int)F.size() - 1;
+        int p = F.at(t).at(x), a;
+        if (p == x) return x;
+        else { a = root(p, t); if (a != p) F[t].replace(x, a); return a; }
+    }
+    int size(int x, int t = -1) { return -(t < 0 ? F.back() : F.at(t)).at(x); }
+    bool same(int x, int y, int t = -1) { return root(x, t) == root(y, t); }
     bool unite(int x, int y) {
+        F.emplace_back(F.back());
         x = root(x), y = root(y);
         if (x != y) {
-            if (V[y] < V[x]) swap(x, y);
-            V[x] += V[y], V[y] = x;
+            int sx = size(x), sy = size(y);
+            if (sy > sx) swap(x, y);
+            F.back().replace(x, -sx - sy);
+            F.back().replace(y, x);
         }
         return x != y;
     }
