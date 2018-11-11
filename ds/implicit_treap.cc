f40e626becfc8a01fe7831326870e5c6f986bd44
kongroo <imjcqt@gmail.com>
2018-11-11 13:44:59 +0800

    add persistent treap

--- ds/implicit_treap.cc
+++ ds/implicit_treap.cc
@@ -4,26 +4,26 @@ using namespace std;
 
 namespace ITreap {
 using T = int;
-struct Node { T x; unique_ptr<Node> l, r; int p = rand(), c = 1; bool b = 0; Node(T x) : x(x) {} };
-using P = unique_ptr<Node>;
+struct Node { T x; Node *l = 0, *r = 0; int p = rand(), c = 1; bool b = 0; Node(T x) : x(x) {} };
+using P = Node *;
 
-int cnt(P &t) { return !t ? 0 : t->c; }
-void up(P &t) { if (t) t->c = 1 + cnt(t->l) + cnt(t->r); }
-void push(P &t) { if (t && t->b) {t->b = 0, swap(t->l, t->r); if (t->l) t->l->b ^= 1; if (t->r) t->r->b ^= 1;}}
-T at(P &t, int i) { push(t); int u = cnt(t->l); return i == u ? t->x : i < u ? at(t->l, i) : at(t->r, i - ++u); }
-void split(P &t, P &l, P &r, int k, int a = 0) {
-    !t ? (l.release(), r.release(), void()) : (push(t), k <= a + cnt(t->l)) ?
-    (r = move(t), split(r->l, l, r->l, k, a), up(r)) :
-    (l = move(t), split(l->r, l->r, r, k, a + 1 + cnt(l->l)), up(l));
+int cnt(P t) { return !t ? 0 : t->c; }
+void up(P t) { if (t) t->c = 1 + cnt(t->l) + cnt(t->r); }
+void push(P t) { if (t && t->b) {t->b = 0, swap(t->l, t->r); if (t->l) t->l->b ^= 1; if (t->r) t->r->b ^= 1;}}
+T at(P t, int i) { push(t); int u = cnt(t->l); return i == u ? t->x : i < u ? at(t->l, i) : at(t->r, i - ++u); }
+void split(P t, P &l, P &r, int k, int a = 0) {
+    !t ? void(l = r = 0) : (push(t), k <= a + cnt(t->l)) ?
+    (r = t, split(t->l, l, r->l, k, a), up(r)) :
+    (l = t, split(t->r, l->r, r, k, a + 1 + cnt(t->l)), up(l));
 }
-void merge(P &t, P &l, P &r) {
-    push(l), push(r), !l || !r ? void(t = move(l ? l : r)) : l->p > r->p ?
-    (merge(l->r, l->r, r), up(t = move(l))) : (merge(r->l, l, r->l), up(t = move(r)));
+void merge(P &t, P l, P r) {
+    push(l), push(r), !l || !r ? void(t = l ? l : r) : l->p > r->p ?
+    (merge(l->r, l->r, r), up(t = l)) : (merge(r->l, l, r->l), up(t = r));
 }
-void insert(P &t, int i, T x) { P u, v, w = make_unique<Node>(x); split(t, u, v, i); merge(u, u, w); merge(t, u, v); }
+void insert(P &t, int i, T x) { P u, v; split(t, u, v, i); merge(u, u, new Node(x)); merge(t, u, v); }
 void erase(P &t, int i) {
-    cnt(t->l) == i ? merge(t, t->l, t->r) :
-    cnt(t->l) > i ? erase(t->l, i) : erase(t->r, i - cnt(t->l) - 1), up(t);
+    if (t) cnt(t->l) == i ? merge(t, t->l, t->r) :
+        cnt(t->l) > i ? erase(t->l, i) : erase(t->r, i - cnt(t->l) - 1), up(t);
 }
 void reverse(P &t, int l, int r) {
     P u, v, w;
--- ds/ptreap.cc
+++ ds/ptreap.cc
@@ -2,7 +2,7 @@
 using namespace std;
 
 
-namespace Treap {
+namespace PTreap {
 using T = int;
 struct Node { T x; Node *l = 0, *r = 0; int p = rand(), c = 1; Node(T x) : x(x) {} };
 using P = Node *;
@@ -14,33 +14,40 @@ int index(P t, T x) { return !t ? 0 : x <= t->x ? index(t->l, x) : cnt(t->l) + 1
 int rindex(P t, T x) { return !t ? 0 : x >= t->x ? rindex(t->r, x) : cnt(t->r) + 1 + rindex(t->l, x); }
 T at(P t, int i) { int u = cnt(t->l); return i == u ? t->x : i < u ? at(t->l, i) : at(t->r, i - ++u); }
 void split(P t, T x, P &l, P &r) {
+    P u = t ? new Node(*t) : nullptr;
     !t ? void(l = r = 0) : x < t->x ?
-    (r = t, split(t->l, x, l, r->l), up(r)) : (l = t, split(t->r, x, l->r, r), up(l));
+    (r = u, split(t->l, x, l, r->l), up(r)) : (l = u, split(t->r, x, l->r, r), up(l));
 }
 void merge(P &t, P l, P r) {
     !l || !r ? void(t = l ? l : r) : l->p > r->p ?
     (merge(l->r, l->r, r), up(t = l)) : (merge(r->l, l, r->l), up(t = r));
 }
 void insert(P &t, T x) { P u, v; split(t, x, u, v); merge(u, u, new Node(x)); merge(t, u, v); }
-void erase(P &t, T x) { if (t) t->x == x ? merge(t, t->l, t->r) : erase(x < t->x ? t->l : t->r, x), up(t); }
+void erase(P &t, T x) {
+    if (t) t->x == x ? merge(t, t->l, t->r) :
+        (t = new Node(*t), erase(x < t->x ? t->l : t->r, x)), up(t);
+}
 };
 
 
 // Luogu. 3369
 int main() {
-    using namespace Treap;
+    using namespace PTreap;
     int n;
     scanf("%d", &n);
-    P tr = nullptr;
-    while (n--) {
-        int t, x;
-        scanf("%d%d", &t, &x);
-        if (t == 1) insert(tr, x);
-        else if (t == 2) erase(tr, x);
-        else if (t == 3) printf("%d\n", index(tr, x) + 1);
-        else if (t == 4)  printf("%d\n", at(tr, x - 1));
-        else if (t == 5) printf("%d\n", at(tr, index(tr, x) - 1));
-        else printf("%d\n", at(tr, cnt(tr) - rindex(tr, x)));
+    vector<P> R;
+    R.push_back(nullptr);
+    const int INF = 2147483647;
+    for (int i = 1; i <= n; i++) {
+        int v, o, x;
+        scanf("%d%d%d", &v, &o, &x);
+        R.push_back(R[v]);
+        if (o == 1) insert(R.back(), x);
+        else if (o == 2) erase(R.back(), x);
+        else if (o == 3) printf("%d\n", index(R.back(), x) + 1);
+        else if (o == 4) printf("%d\n", at(R.back(), x - 1));
+        else if (o == 5) printf("%d\n", index(R.back(), x) ? at(R.back(), index(R.back(), x) - 1) : -INF);
+        else printf("%d\n", rindex(R.back(), x) ? at(R.back(), cnt(R.back()) - rindex(R.back(), x)) : INF);
     }
     return 0;
 }
