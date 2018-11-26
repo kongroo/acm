f8c3aebddf39893cec4bd262b02e1bb8013ed594
kongroo <imjcqt@gmail.com>
2018-11-26 16:08:07 +0800

    add mod to string hash

--- strings/hash_1.cc
+++ strings/hash_1.cc
@@ -2,42 +2,40 @@
 using namespace std;
 
 
-namespace Hash1 {
-using ULL = unsigned long long;
-ULL seed(131);
-int n = 0;
-vector<ULL> P, H;
-template <typename T> void init(const T& S) {
-    n = S.size();
-    P.resize(n);
-    H.resize(n + 1);
-    P[0] = 1, H[n] = 0;
-    for (int i = 1; i < n; i++) P[i] = P[i - 1] * seed;
-    for (int i = n - 1; i >= 0; i--) H[i] = H[i + 1] * seed + S[i];
-}
-ULL get(int l, int r) { return H[l] - H[r] * P[r - l]; }
-}
+struct Hash1 {
+    int n, p, m;
+    vector<int> P, H;
+    constexpr int mul(int x, int y, int m) { return 1LL * x * y % m; }
+    constexpr int add(int x, int y, int m) { int t = x + y; return t < 0 ? t + m : t < m ? t : t - m; }
+    template <typename T> Hash1(const T &S, int p = 131, int m = 1000000409) :
+        n(S.size()), p(p), m(m), P(n), H(n + 1) {
+        P[0] = 1, H[n] = 0;
+        for (int i = 1; i < n; i++) P[i] = mul(P[i - 1], p, m);
+        for (int i = n - 1; i >= 0; i--) H[i] = add(mul(H[i + 1], p, m), S[i],  m);;
+    }
+    int get(int l, int r) { return add(H[l], -mul(H[r], P[r - l], m), m); }
+};
 
 
 // CF. 126B
 int main() {
     string s;
     cin >> s;
-    Hash1::init(s);
+    Hash1 hs(s);
     int n = s.size();
     auto fail = []() { return puts("Just a legend"), 0; };
     if (n < 3) return fail();
     auto check = [&](int len) {
-        auto t = Hash1::get(0, len);
+        auto t = hs.get(0, len);
         for (int i = 1; i < n - len; i++) {
-            if (Hash1::get(i, i + len) == t) return true;
+            if (hs.get(i, i + len) == t) return true;
         }
         return false;
     };
     vector<int> L;
     for (int len = 1; len < n - 1; len++) {
         int l = 0, r = n - len;
-        auto t = Hash1::get(l, l + len), u = Hash1::get(r, r + len);
+        auto t = hs.get(l, l + len), u = hs.get(r, r + len);
         if (t == u) L.push_back(len);
     }
     int l = 0, r = (int)L.size();
--- strings/hash_2.cc
+++ strings/hash_2.cc
@@ -2,47 +2,49 @@
 using namespace std;
 
 
-namespace Hash2 {
-using ULL = unsigned long long;
-ULL p1(131), p2(1313);
-int n = 0;
-vector<ULL> H[4];
-template <typename T> void init(const T& S) {
-    n = S.size();
-    for (int i = 0; i < 4; i++) H[i].assign(n + 1, 0ULL);
-    H[2][0] = H[3][0] = 1;
-    for (int i = 1; i < n; i++) H[2][i] = H[2][i - 1] * p1, H[3][i] = H[3][i - 1] * p2;
-    for (int i = n - 1; i >= 0; i--)
-        H[0][i] = H[0][i + 1] * p1 + S[i], H[1][i] = H[1][i + 1] * p2 + S[i];
-}
-pair<ULL, ULL> get(int l, int r) {
-    return {
-        H[0][l] - H[0][r]* H[2][r - l],
-        H[1][l] - H[1][r]* H[3][r - l]
-    };
-}
-}
+struct Hash2 {
+    int n, p1, p2, m1, m2;
+    vector<int> H[4];
+    constexpr int mul(int x, int y, int m) { return 1LL * x * y % m; }
+    constexpr int add(int x, int y, int m) { int t = x + y; return t < 0 ? t + m : t < m ? t : t - m; }
+    template <typename T> Hash2(const T &S, int p1 = 131, int p2 = 1313, int m1 = 1e9 + 7,
+                                int m2 = 1e9 + 9): n(S.size()), p1(p1), p2(p2), m1(m1), m2(m2) {
+        for (int i = 0; i < 4; i++) H[i].assign(n + 1, 0ULL);
+        H[2][0] = H[3][0] = 1;
+        for (int i = 1; i < n; i++) H[2][i] = mul(H[2][i - 1], p1, m1), H[3][i] = mul(H[3][i - 1], p2, m2);
+        for (int i = n - 1; i >= 0; i--) {
+            H[0][i] = add(mul(H[0][i + 1], p1, m1), S[i], m1);
+            H[1][i] = add(mul(H[1][i + 1], p2, m2), S[i], m2);
+        }
+    }
+    pair<int, int> get(int l, int r) {
+        return {
+            add(H[0][l], -mul(H[0][r], H[2][r - l], m1), m1),
+            add(H[1][l], -mul(H[1][r], H[3][r - l], m2), m2)
+        };
+    }
+};
 
 
 // CF. 126B
 int main() {
     string s;
     cin >> s;
-    Hash2::init(s);
+    Hash2 hs(s);
     int n = s.size();
     auto fail = []() { return puts("Just a legend"), 0; };
     if (n < 3) return fail();
     auto check = [&](int len) {
-        auto t = Hash2::get(0, len);
+        auto t = hs.get(0, len);
         for (int i = 1; i < n - len; i++) {
-            if (Hash2::get(i, i + len) == t) return true;
+            if (hs.get(i, i + len) == t) return true;
         }
         return false;
     };
     vector<int> L;
     for (int len = 1; len < n - 1; len++) {
         int l = 0, r = n - len;
-        auto t = Hash2::get(l, l + len), u = Hash2::get(r, r + len);
+        auto t = hs.get(l, l + len), u = hs.get(r, r + len);
         if (t == u) L.push_back(len);
     }
     int l = 0, r = (int)L.size();
