5ce4d505a50194c94369683d52eaeba5bd7c940e
kongroo <imjcqt@gmail.com>
2019-01-10 23:36:08 +0800

    .

---
+++ graphs/dominator_tree.cc
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+vector<vector<int>> get_dominator_tree(const vector<vector<int>> &G, int root) {
+    int n = (int)G.size(), sz = 0;
+    vector<int> In(n, -1), S(n), Idom(n), Semi(n), B(n), Fa(n), Pre(n);
+    vector<vector<int>> Dom(n), rG(n), R(n);
+    for (int u = 0; u < n; u++) for (auto v : G[u]) rG[v].push_back(u);
+    for (int u = 0; u < n; u++) Fa[u] = B[u] = Semi[u] = u;
+    auto get = [&](int x, auto f) -> int {
+        if (x == Fa[x]) return x;
+        int y = f(Fa[x], f);
+        if (Semi[B[Fa[x]]] < Semi[B[x]]) B[x] = B[Fa[x]];
+        return Fa[x] = y;
+    };
+    auto dfs = [&](int u, auto f) -> void {
+        In[u] = sz, S[sz++] = u;
+        for (auto v : G[u]) if (!~In[v]) f(v, f), Pre[In[v]] = In[u];
+    };
+    dfs(root, dfs);
+    for (int i = sz - 1, u, v; u = S[i], i > 0; --i) {
+        for (auto rv : rG[u]) if (~(v = In[rv]))
+                get(v, get), Semi[i] = min(Semi[i], Semi[B[v]]);
+        Dom[Semi[i]].push_back(i);
+        int x = Fa[i] = Pre[i];
+        for (auto z : Dom[x])
+            get(z, get), Idom[z] = Semi[B[z]] < x ? B[z] : x;
+        Dom[x].clear();
+    }
+    for (int i = 1; i < sz; i++) {
+        if (Semi[i] != Idom[i]) Idom[i] = Idom[Idom[i]];
+        Dom[Idom[i]].push_back(i);
+    }
+    for (int u = 0; u < n; u++) for (auto v : Dom[u]) R[S[u]].push_back(S[v]);
+    return R;
+}
+
+
+// Luogu. 5180
+int main() {
+    int n, m;
+    scanf("%d%d", &n, &m);
+    vector<vector<int>> G(n + 1);
+    while (m--) {
+        int u, v;
+        scanf("%d%d", &u, &v);
+        G[u].push_back(v);
+    }
+    auto R = get_dominator_tree(G, 1);
+    vector<int> F(n + 1);
+    auto dfs = [&](int u, int p, auto f) -> void {
+        F[u] = 1;
+        for (auto v : R[u]) if (v != p)
+                f(v, u, f), F[u] += F[v];
+    };
+    dfs(1, 0, dfs);
+    for (int i = 1; i <= n; i++) printf("%d%c", F[i], " \n"[i == n]);
+}
