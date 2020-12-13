a283fb6380ad80d90da49eddf0185bd2e7be14ef
kongroo <imjcqt@gmail.com>
2020-12-13 20:42:01 +0800

    update template

--- template.cc
+++ template.cc
@@ -1,9 +1,8 @@
 #include <bits/stdc++.h>
 using namespace std;
 using ll = long long;
+using pii = pair<int, int>;
 using pll = pair<ll, ll>;
-#define FI(i, a, b) for (int i = (a); i <= (b); ++i)
-#define FD(i, b, a) for (int i = (b); i >= (a); --i)
 #define DEBUG(x) cerr << #x << ": " << (x) << endl;
 
 int main() {
