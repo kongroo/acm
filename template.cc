42471564578dd5f8a802d10a32cc1a8a7bdddf3f
kongroo <imjcqt@gmail.com>
2020-12-13 13:53:41 +0800

    add template

---
+++ template.cc
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using pll = pair<ll, ll>;
+#define FI(i, a, b) for (int i = (a); i <= (b); ++i)
+#define FD(i, b, a) for (int i = (b); i >= (a); --i)
+#define DEBUG(x) cerr << #x << ": " << (x) << endl;
+
+int main() {
+
+}
